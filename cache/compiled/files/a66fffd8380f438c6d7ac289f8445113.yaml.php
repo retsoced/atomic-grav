f05ce8f5427a1c496c7201c7c6cfe42690d84395
Darryll DeCoster <deltaecho3@gmail.com>
2018-06-19 04:52:21 -0700

    Added plugins and updated README
    I added a couple of new plugins, Featherlight (lightbox) and SEO.

--- cache/compiled/files/a66fffd8380f438c6d7ac289f8445113.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/deltaecho3/Development/grav-pattern-library-template/user/config/media.yaml',
-    'modified' => 1527057991,
-    'data' => [
-        
-    ]
-];
